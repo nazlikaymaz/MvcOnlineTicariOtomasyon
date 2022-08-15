d10a580d8e4ea9939f2044fb60686aa353cd97e6
Nazlı Kaymaz <kaymaznazli4321.1@gmail.com>
2022-08-16 00:53:08 +0300

    Add project files.

---
+++ Models/Sınıflar/Cariler.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace MvcOnlineTicariOtomasyon.Models.Sınıflar
+{
+    public class Cariler
+    {
+        [Key]
+        public int CariID { get; set; }
+
+        [Column(TypeName = "VarChar")]
+        [StringLength(30)]
+        public string CariAd { get; set; }
+
+        [Column(TypeName = "VarChar")]
+        [StringLength(30)]
+        public string CariSoyad { get; set; }
+
+        [Column(TypeName = "VarChar")]
+        [StringLength(13)]
+        public string CariSehir { get; set; }
+
+        [Column(TypeName = "VarChar")]
+        [StringLength(50)]
+        public string CariMail { get; set; }
+        public SatisHareket SatisHareket { get; set; }
+    }
+}
---
+++ Models/Sınıflar/SatisHareket.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MvcOnlineTicariOtomasyon.Models.Sınıflar
+{
+    public class SatisHareket
+    {
+        [Key]
+        public int SatisID { get; set; }
+        public DateTime Tarih { get; set; }
+        public int Adet { get; set; }
+        public decimal Fiyat { get; set; }
+        public decimal ToplamTutar { get; set; }
+        public ICollection<Urun> Uruns { get; set; }
+        public ICollection<Cariler> Carilers { get; set; }
+        public ICollection<Personel> Personels { get; set; }
+  
+      
+    }
+}
