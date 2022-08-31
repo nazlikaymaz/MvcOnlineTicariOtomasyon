d22529855bea656514f4991a55cc7a0cdd38b67f
Nazlı Kaymaz <kaymaznazli4321.1@gmail.com>
2022-09-01 02:31:28 +0300

    department deletion and update

--- Controllers/DepartmanController.cs
+++ Controllers/DepartmanController.cs
@@ -13,7 +13,7 @@ namespace MvcOnlineTicariOtomasyon.Controllers
         Context c = new Context();
         public ActionResult Index()
         {
-            var degerler = c.Departmans.ToList();
+            var degerler = c.Departmans.Where(x => x.Durum == true).ToList();
             return View(degerler);
         }
         [HttpGet]
@@ -27,7 +27,25 @@ namespace MvcOnlineTicariOtomasyon.Controllers
             c.Departmans.Add(d);
             c.SaveChanges();
             return RedirectToAction("Index");
-
+        }
+        public ActionResult DepartmanSil(int id)
+        {
+            var dep = c.Departmans.Find(id);
+            dep.Durum = false;
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult DepartmanGetir(int id)
+        {
+            var dpt = c.Departmans.Find(id);
+            return View("DepartmanGetir", dpt);
+        }
+        public ActionResult DepartmanGuncelle(Departman p)
+        {
+            var dept = c.Departmans.Find(p.DepartmanID);
+            dept.DepartmanAd = p.DepartmanAd;
+            c.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
