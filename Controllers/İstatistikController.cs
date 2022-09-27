a8184e29fd4dee5a1b768abe03431f55f156638f
Nazlı Kaymaz <kaymaznazli4321.1@gmail.com>
2022-09-28 02:57:40 +0300

    statistics

---
+++ Controllers/İstatistikController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcOnlineTicariOtomasyon.Models.Siniflar;
+
+namespace MvcOnlineTicariOtomasyon.Controllers
+{
+    public class İstatistikController : Controller
+    {
+        // GET: İstatistik
+        Context c = new Context();
+        public ActionResult Index()
+        {
+            var deger1 = c.Carilers.Count().ToString();
+            ViewBag.d1 = deger1;
+            var deger2 = c.Uruns.Count().ToString();
+            ViewBag.d2 = deger2;
+            var deger3 = c.Personels.Count().ToString();
+            ViewBag.d3 = deger3;
+            var deger4 = c.Kategoris.Count().ToString();
+            ViewBag.d4 = deger4;
+            var deger5 = c.Uruns.Sum(x => x.Stok).ToString();
+            ViewBag.d5 = deger5;
+            var deger6 = (from x in c.Uruns select x.Marka).Distinct().Count().ToString();
+            ViewBag.d6 = deger6;
+            var deger7 = c.Uruns.Count(x => x.Stok <= 20).ToString();
+            ViewBag.d7 = deger7;
+            var deger8 = (from x in c.Uruns orderby x.SatisFiyat descending select x.UrunAd).FirstOrDefault();
+            ViewBag.d8 = deger8;
+            return View();
+        }
+    }
+}
