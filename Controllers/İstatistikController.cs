1c4c5dc3fa3b65331ec7a1b7db6a22be3e7d74ef
Nazlı Kaymaz <kaymaznazli4321.1@gmail.com>
2022-10-02 17:26:24 +0300

    fix

--- Controllers/İstatistikController.cs
+++ Controllers/İstatistikController.cs
@@ -29,6 +29,23 @@ namespace MvcOnlineTicariOtomasyon.Controllers
             ViewBag.d7 = deger7;
             var deger8 = (from x in c.Uruns orderby x.SatisFiyat descending select x.UrunAd).FirstOrDefault();
             ViewBag.d8 = deger8;
+            var deger9 = (from x in c.Uruns orderby x.SatisFiyat ascending select x.UrunAd).FirstOrDefault();
+            ViewBag.d9 = deger9;
+            var deger10 = c.Uruns.Count(x => x.UrunAd == "Buzdolabı").ToString();
+            ViewBag.d10 = deger10;
+            var deger11 = c.Uruns.Count(x => x.UrunAd == "Laptop").ToString();
+            ViewBag.d11 = deger11;
+            var deger12 = c.Uruns.GroupBy(x => x.Marka).OrderByDescending(z => z.Count()).Select(y => y.Key).FirstOrDefault();
+            ViewBag.d12 = deger12;
+            var deger13 = c.Uruns.Where(u => u.UrunID == (c.SatisHarekets.GroupBy(x => x.UrunID).OrderByDescending(z => z.Count()).Select(y => y.Key).FirstOrDefault())).Select(k => k.UrunAd).FirstOrDefault();
+            ViewBag.d13 = deger13;
+            var deger14 = c.SatisHarekets.Sum(x => x.ToplamTutar).ToString();
+            ViewBag.d14 = deger14;
+            DateTime bugun = DateTime.Today;
+            var deger15 = c.SatisHarekets.Count(x => x.Tarih == bugun).ToString();
+            ViewBag.d15 = deger15;
+            var deger16 = c.SatisHarekets.Where(x => x.Tarih == bugun).Sum(y => y.ToplamTutar).ToString();
+            ViewBag.d16 = deger16;
             return View();
         }
     }
